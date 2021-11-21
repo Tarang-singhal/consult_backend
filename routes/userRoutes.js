ddd84ad1006e9d492c71a799b70bb25b79b97d56
Atishey jain <atisheyjain1232@gmail.com>
2021-11-21 14:23:36 +0530

    Created filter and query functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 // const userController = require("./../controllers/userController");
-const authController = require("./../controllers/authController");
+const userController = require("./../controllers/userController.js");
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
+router.get("/", userController.getUsers);
 // router.get("/logout", authController.logout);
 
 // router.post("/forgotPassword", authController.forgotPassword);
